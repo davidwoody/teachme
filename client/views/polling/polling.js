de8c9709c49585c547a8b5d593b89dbdb46397e4
davidwoody <davidjwoody@gmail.com>
2014-05-04 03:21:48 -0400

    added classes

--- client/views/polling/polling.js
+++ client/views/polling/polling.js
@@ -1,15 +1,15 @@
 Template.polling.helpers({
 
 
-//   theQuestion: function(){
-//     var questionId = Router.current().params.questionId;
-//     // WHY CAN I NOT ACCESS THE PROPERIES RETURNED?
-//     // SOMETIMES WRONG
-//     return Questions.findOne({_id: questionId});
-//   },
-//   number: function(){
-//     return Router.current().params.listNumber;
-//   },
+  theQuestion: function(){
+    var questionId = Router.current().params.questionId;
+    // WHY CAN I NOT ACCESS THE PROPERIES RETURNED?
+    // SOMETIMES WRONG
+    return Questions.findOne({_id: questionId});
+  },
+  number: function(){
+    return Router.current().params.listNumber;
+  },
 // <<<<<<< HEAD
 
 //   responsesYes: function(){
