d9f6196fcc800583d354f99740479f01658e638c
davidwoody <davidjwoody@gmail.com>
2014-05-04 07:17:16 -0400

    Buttons are now dynamically added

--- client/views/responding/responding.js
+++ client/views/responding/responding.js
@@ -13,6 +13,23 @@ Template.responding.helpers({
           }
       }
       return null;
+    },
+    // Quizy
+    answerOptions: function(){
+      
+      var listNumber = parseInt(Router.current().params.listNumber, 10);
+      var questionList = QuestionsList.find({ number: listNumber }).fetch();
+      if (questionList && questionList.length > 0) {
+          var questionId = questionList[0].currentQuestion;
+          var question = Questions.find({ _id: questionId }).fetch();
+          if (question && question.length > 0) {
+            // $('.add-response').prop('disabled', false).removeClass('btn-default');
+            // $("#answerYes").addClass('btn-success');
+            // $("#answerNo").addClass('btn-danger');
+            return question[0].answerOptions;
+          }
+      }
+      // return null;
     }
 });
 
@@ -26,7 +43,7 @@ Template.responding.events({
     {
       Meteor.call('createResponse', questionList.currentQuestion, button.data('answer'));
     }
-    $(".add-response").not(button).removeClass("btn-success").removeClass("btn-danger").addClass('btn-default');
+    // $(".add-response").not(button).removeClass("btn-success").removeClass("btn-danger").addClass('btn-default');
     $('.add-response').prop('disabled', true);
   }
 }); //events
